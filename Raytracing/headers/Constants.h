d981eec6707824a8800e6ef5ebaf929a2c305f86
Tom <t.rakoto76@gmail.com>
2021-07-20 19:30:13 +0200

    ignore Constant.h

--- Raytracing/headers/Constants.h
+++
@@ -1,9 +0,0 @@
-#ifndef RT_CONSTANTS_H
-#define RT_CONSTANTS_H
-
-#include <string>
-
-// A cause de problèmes de chemins relatifs, nous utilisons un chemin absolu
-const std::string projectDir = "D:\\Raytracing\\";
-
-#endif
