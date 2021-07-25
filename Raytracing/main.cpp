d0cf74b273274c37f06b38bc64f311c9095f296e
Tom <t.rakoto76@gmail.com>
2021-07-25 19:37:50 +0200

    shadow

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -55,7 +55,7 @@ DataContext data;
 
 void renderCallback(DataContext data)
 {
-	auto texture = std::make_shared<Texture>("D:\\Raytracing\\resources\\sample.jpg");
+	auto texture = std::make_shared<Texture>("D:\\Dev\\GPUdev\\ESGI\\Raytracing\\resources\\sample.jpg");
 	// prepare to render 
 	auto material1 = std::make_shared<Material>(Color::white, Color::white, Color::white * 0.2, 1);
 	_sleep(0.000001);
@@ -63,29 +63,29 @@ void renderCallback(DataContext data)
 	material2->texture = *texture;
 	auto blue = std::make_shared<Material>(Color(0, 0, 1), Color(0, 0, 1), Color(0, 0, 1), 1);
 	auto red = std::make_shared<Material>(Color(1, 0, 0), Color(1, 0, 0), Color(1, 0, 0), 1);
-	auto pos = Vector(-2, 2.4, -35);
-	auto pos2 = Vector(1, 3.5, -25);
+	
 	auto ground = std::make_shared<Square>(Vector(0, -1.2, -11), Vector(90, 0, 0), 2, material1);
-	auto plane = std::make_shared<Plane>(Vector(0, 0, -50), Vector(0, 90, 0), 2, blue);
+	auto plane = std::make_shared<Plane>(Vector(0, 0, -50), Vector(0, 0, 0), 2, blue);
 	auto leftWall = std::make_shared<Square>(Vector(-2.1, 0, -10), Vector(0, 80, 0), 3, red);
 	auto rightWall = std::make_shared<Square>(Vector(2.1, 0, -10), Vector(0, -80, 0), 3, red);
-	auto s2 = std::make_shared<Sphere>(Vector(0, 0, 0 - 11), Vector(), .4f, material2);
-	auto cone = std::make_shared<Cone>(Vector(0, 0, 0 - 11), Vector(), .1f, material2);
-	auto cy1 = std::make_shared<Cylinder>(Vector(0, 0, 0 - 11), Vector(0., 0.5, 1), 0.5, material2);
+	auto s1 = std::make_shared<Sphere>(Vector(-1, 0, 0 - 11), Vector(), .8f, blue);
+	auto s2 = std::make_shared<Sphere>(Vector(0.5, 0, 0 - 11), Vector(), .4f, material1);
+	/*auto cone = std::make_shared<Cone>(Vector(0, 0, 0 - 11), Vector(), .1f, material2);
+	auto cy1 = std::make_shared<Cylinder>(Vector(0, 0, 0 - 11), Vector(0., 0.5, 1), 0.5, material2);*/
 	auto t1 = std::make_shared<Triangle>(Vector(-1, 0, -20), Vector(90, 0, 0), 1.7, material1);
-	auto l1 = std::make_shared<PointLight>(Vector(0.2f, 0, -9), Vector(0, 0, 0), Color(1, 0, 0), Color::white * 0.4f, 1.f);
-	auto l2 = std::make_shared<AmbientLight>(Vector(-0.2f, -0.7, -10), Vector(0, 0, 0), Color(0, 0, 1), Color::white * 0.4f, 1.f);
+//	auto l1 = std::make_shared<PointLight>(Vector(0.2f, 0, -9), Vector(0, 0, 0), Color(1, 0, 0), Color::white * 0.4f, 1.f);
+	auto l2 = std::make_shared<AmbientLight>(Vector(10, 0, -11), Vector(0, 0, 0), Color::white, Color::white * 0.4f, 1.f);
 	auto scene = std::make_shared<Scene>(Color::blue * 0.1 + Color::red * 0.15, Color::white * 0.4);
 	auto camera = std::make_shared<Camera>(10);
 
 	scene->add(texture);
 	scene->add(material1);
 	scene->add(material2);
+//	scene->add(ground);
 	scene->add(s2);
-	scene->add(plane);
-	scene->add(leftWall);
-	scene->add(rightWall);
-	scene->add(l1);
+	scene->add(s1);
+	//scene->add(l1);
+	scene->add(l2);
 	/*
 	std::string pathWrite = "D:\\Raytracing\\SerializedData.txt";
 	std::string pathRead = "D:\\Raytracing\\sampleScene.txt";
