bbe28505e0e25a150fe9c40bbe1e54226a54df29
Tom <t.rakoto76@gmail.com>
2021-07-25 10:52:46 +0200

    add intersection cone

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -38,6 +38,7 @@
 #include "headers/primitives/Square.h"
 #include "headers/primitives/Cylinder.h"
 #include "headers/primitives/Triangle.h"
+#include "headers/primitives/Cone.h"
 #include "headers/engine/Serializer.h"
 
 #include "imgui.h"
@@ -55,37 +56,40 @@ struct DataContext {
 
 void renderCallback(float w, float h, int ssaSub, bool shadowActivated, Illumination illuminationModel, std::string savePath, std::string fileName)
 {
-	std::string texture = "E:\\dev\\Raytracing\\resources\\sample.jpg";
+	std::string texture = "D:\\Dev\\GPUdev\\ESGI\\Raytracing\\resources\\sample.jpg";
 	// prepare to render 
 	auto material1 = std::make_shared<Material>(Color::white, Color::white, Color::white * 0.2, 1);
 	auto material2 = std::make_shared<Material>(Color::white, Color::white, Color::white, 1);
-	auto blue = std::make_shared<Material>(Color(0,0,1), Color(0, 0, 1), Color(0, 0, 1), 1);
-	auto red = std::make_shared<Material>(Color(1,0,0), Color(1, 0, 0), Color(1, 0, 0), 1);
+	auto blue = std::make_shared<Material>(Color(0, 0, 1), Color(0, 0, 1), Color(0, 0, 1), 1);
+	auto red = std::make_shared<Material>(Color(1, 0, 0), Color(1, 0, 0), Color(1, 0, 0), 1);
 	material2->texture = std::make_shared<Image>(texture);
 	auto pos = Vector(-2, 2.4, -35);
 	auto pos2 = Vector(1, 3.5, -25);
 	//auto s1 = std::make_shared<Cube>(pos, Vector(0.2, 0.7, 0), 1.7, material2);
-	auto ground = std::make_shared<Square>(Vector(0,-1,-11), Vector(90,0,0), 2, material1);
-	auto leftWall = std::make_shared<Square>(Vector(-2.1,0,-10), Vector(0,80,0), 2, blue);
-	auto rightWall = std::make_shared<Square>(Vector(2.1,0,-10), Vector(0,-80,0), 2, red);
-	auto s2 = std::make_shared<Sphere>(Vector(0,0,0-11), Vector(), .4f, material2);
+	auto ground = std::make_shared<Square>(Vector(0, -1.2, -11), Vector(90, 0, 0), 2, material1);
+	auto plane = std::make_shared<Plane>(Vector(0, 0, -50), Vector(0, 90, 0), 2, blue);
+	auto leftWall = std::make_shared<Square>(Vector(-2.1, 0, -10), Vector(0, 80, 0), 3, red);
+	auto rightWall = std::make_shared<Square>(Vector(2.1, 0, -10), Vector(0, -80, 0), 3, red);
+	auto s2 = std::make_shared<Sphere>(Vector(0, 0, 0 - 11), Vector(), .4f, material2);
+	auto cone = std::make_shared<Cone>(Vector(0, 0, 0 - 30), Vector(), 1.0f, material2);
 	auto cy1 = std::make_shared<Cylinder>(Vector(0, 2, -15), Vector(0., 0.5, 1), 0.5, material2);
 	auto t1 = std::make_shared<Triangle>(Vector(-1, 0, -20), Vector(90, 0, 0), 1.7, material1);
-	auto l1 = std::make_shared<PointLight>(Vector(0.2f, -0.7, -9), Vector(0, 0, 0), Color(1, 0, 0), Color(1,0,0), 1.f);
-	auto l2 = std::make_shared<PointLight>(Vector(-0.2f, 0, -10), Vector(0, 0, 0), Color(0, 0, 1), Color::white*0.4f, 1.f);
-	//auto l2 = std::make_shared<AmbientLight>(Vector(-15, 0, 50), Vector(0, 0, 0), Color::white, Color::white * 0.2, .2f);
+	auto l1 = std::make_shared<PointLight>(Vector(0.2f, 0, -9), Vector(0, 0, 0), Color(1, 0, 0), Color::white * 0.4f, 1.f);
+	auto l2 = std::make_shared<PointLight>(Vector(-0.2f, -0.7, -10), Vector(0, 0, 0), Color(0, 0, 1), Color::white * 0.4f, 1.f);
 	auto scene = std::make_shared<Scene>(Color::blue * 0.1 + Color::red * 0.15, Color::white * 0.4);
 	auto camera = std::make_shared<Camera>(10);
 
-	scene->add(0,texture);
-	scene->add(material1);
+	scene->add(0, texture);
+	scene->add(cone);
+	/*scene->add(material1);
 	scene->add(material2);
 	scene->add(s2);
+	scene->add(plane);
 	scene->add(ground);
-    scene->add(leftWall);
+	scene->add(leftWall);
 	scene->add(rightWall);
 	scene->add(l1);
-	//scene->add(l2);
+	scene->add(l2);*/
 
 	auto serializer = Serializer();
 	serializer.serializeScene(scene);
@@ -179,11 +183,11 @@ void ImGUICallback()
 	ImGui::InputTextWithHint("Enter scene name", "scene name", sceneName, IM_ARRAYSIZE(sceneName));
 	ImGui::InputTextWithHint("Enter scene path", "scene path", scenePath, IM_ARRAYSIZE(scenePath));
 
-	if (ImGui::Button("-- LOAD SCENE --")) 
+	if (ImGui::Button("-- LOAD SCENE --"))
 	{
 		//loadSceneCallBack(sceneName, scenePath);
 	}
-	if (ImGui::Button("-- SAVE SCENE --")) 
+	if (ImGui::Button("-- SAVE SCENE --"))
 	{
 		//saveceneCallBack(sceneName, scenePath);
 	}
--- Raytracing/src/primitives/Cone.cpp
+++ Raytracing/src/primitives/Cone.cpp
@@ -1,4 +1,4 @@
-#include "../../headers/primitives/Cone.h"
+﻿#include "../../headers/primitives/Cone.h"
 
 Cone::Cone(const Vector& position, const Vector& rotation, float scale, const std::shared_ptr<Material>& material) :
 	Object(position, rotation, scale, material)
@@ -6,13 +6,47 @@ Cone::Cone(const Vector& position, const Vector& rotation, float scale, const st
 
 bool Cone::intersect(const Ray& ray, Point& impact) const
 {
+	/* Cone Infini :
+	 
+	     v_a = ligne verticale (normalisé) passant par l'apex 
 
-	Ray lr = globalToLocal(ray).normalized();
-	if (lr.vector[2]<0.0001 && lr.vector[2]>-0.0001)return false;
-	if (lr.vector[2] * lr.origin[2] > 0)return false;
-	float t = -lr.origin[2] / lr.vector[2];
-	Point p = lr.origin + t * lr.vector;
-	if (p[0] < -1 || p[0]>1 || p[1] < -1 || p[1]>1)return false;
+		\ | / alpha = demi angle ( angle entre v_a et l'un des côtés du cone
+		 \|/
+		 / \ apex = sommet du milieu
+		/   \
+
+	  Un point p(x,y,z) appartient au cone ssi :
+	  cos²(alpha) * (p - apex -(v_a, p-apex)v_a²)²-sin²(alpha)(v_a, p-apex)² = 0  , où v_a et la ligne vertical passant par l'apex 
+
+	  Sur le rayon on caractérise le point cherché par p(t) = O - D*t
+
+	  d'où on a : 
+	  cos²(alpha) * (D*t + O - apex -(v_a, D*t + O-apex)v_a²)²-sin²(alpha)(v_a, D*t+O-apex)² = 0 
+
+	  Ensuite resolvation de l'équation du second degré de la même manière que la sphere.
+
+	*/
+
+	Ray r = globalToLocal(ray).normalized();
+
+	Vector Va = Vector(0, 1, 0).normalized();
+	float alpha = 0.95f;
+	float A = Vector::dot(r.vector, Va) * Vector::dot(r.vector, Va) - alpha * alpha; 
+	float B = 2. * (Vector::dot(r.origin, Va) - Vector::dot(r.vector, r.origin) * alpha * alpha);
+	float C = Vector::dot(r.origin, Va) * Vector::dot(r.origin, Va) - Vector::dot(r.origin, r.origin) * alpha * alpha;
+
+	float delta = B * B - 4. * A * C;
+	if (delta < 0.0001f) return false;
+
+	float sqrtDelta = sqrt(delta);
+	float t1 = (-B - sqrtDelta) / (2. * A);
+	float t2 = (-B + sqrtDelta) / (2. * A);
+
+	float t = t1;
+	if (t < 0. || t2 > 0. && t2 < t) t = t2;
+	if (t < 0.) return false;
+
+	Point p = r.origin + t * r.vector;
 	impact = localToGlobal(p);
 	return true;
 }
